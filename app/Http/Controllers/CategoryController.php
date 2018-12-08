341cbf90daa4b8d23c2b4601e9250ed8efcc727a
Chayan Datta <sonicxxx7@gmail.com>
2018-12-08 15:17:35 +0530

    Category Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Categorie;
 
 class CategoryController extends Controller
 {
-    //
+    public function add(Request $request)
+    {
+        $category = new Categorie();
+        $category->category = $request->category;
+        $category->save();
+        return back()->with('status', 'Category Successfully Added');
+    }
+
+    public function delete($id)
+    {
+        $category = Categorie::where('id', $id)->firstOrFail();
+        $category->delete();
+        return back()->with('status', 'Category Deleted Successfully');
+    }
+
+    public function update($id, Request $request){
+        $category = Categorie::where('id', $id)->firstOrFail();
+        $category->category = $request->category;
+        $category->save();
+        return back()->with('status', 'Category Updated Successfully');
+    }
 }
