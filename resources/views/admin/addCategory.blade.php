504ea464c9d4e1c20a1f40fd8886b8a3b913c2bf
Chayan Datta <sonicxxx7@gmail.com>
2018-12-08 22:22:08 +0530

    Add Page UI fixes

--- resources/views/admin/addCategory.blade.php
+++ resources/views/admin/addCategory.blade.php
@@ -1,6 +1,11 @@
 @extends('admin.layouts.admin')
 @section('page', 'Add Category')
 @section('content')
+    @if(session('status'))
+        <div class="alert alert-success" role="alert">
+            <strong>{{ session('status') }}</strong>
+        </div>
+    @endif
     <div class="card">
         <div class="card-header">
             <strong>Add Category</strong>
@@ -10,7 +15,7 @@
                 @csrf
                 <div class="row form-group">
                     <div class="col col-md-3"><label for="text-input" class=" form-control-label">Category </label></div>
-                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="name" placeholder="Product" class="form-control"><small class="form-text text-muted">Just the Category</small></div>
+                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="category" placeholder="Category" class="form-control"><small class="form-text text-muted">Just the Category</small></div>
                 </div>
         </div>
         <div class="card-footer">
--- resources/views/admin/addCompany.blade.php
+++ resources/views/admin/addCompany.blade.php
@@ -1,6 +1,11 @@
 @extends('admin.layouts.admin')
 @section('page', 'Add Company')
 @section('content')
+    @if(session('status'))
+        <div class="alert alert-success" role="alert">
+            <strong>{{ session('status') }}</strong>
+        </div>
+    @endif
     <div class="card">
         <div class="card-header">
             <strong>Add Company</strong>
@@ -10,7 +15,7 @@
                 @csrf
                 <div class="row form-group">
                     <div class="col col-md-3"><label for="text-input" class=" form-control-label">Company </label></div>
-                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="name" placeholder="Product" class="form-control"><small class="form-text text-muted">Just the Company</small></div>
+                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="company" placeholder="Company" class="form-control"><small class="form-text text-muted">Just the Company</small></div>
                 </div>
         </div>
         <div class="card-footer">
